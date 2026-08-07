144dd4edf5129d1fda26e516907c321efa333182
CodeKnight <dearjinni44@gmail.com>
2026-08-07 10:15:57 +0545

    feat: add phone, address, photo, gender and dob fields to the user model

--- users/models.py
+++ users/models.py
@@ -4,5 +4,16 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    pass 
+    GENDER_CHOICES = (
+        ("male","MALE"),
+        ("female","FEMALE"),
+        ("other", "OTHER"),
+        ("prefer not to say", "PREFER NOT TO SAY")
+    )
+    
+    phone = models.CharField(max_length=20, null = True, blank=True)
+    address = models.CharField(max_length=200, null = True, blank=True)
+    photo = models.ImageField(upload_to='photo/', null = True, blank=True)
+    dob = models.DateField( null = True, blank=True)
+    gender = models.CharField(max_length=50, choices = GENDER_CHOICES, default = "prefer not to say")
 
